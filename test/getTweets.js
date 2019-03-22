7d1e8132e95cc73b685441484f1850503f71cd0a
Marko Kosir <kosir.mar@gmail.com>
2019-03-22 22:52:28 +0100

    comment added

--- test/getTweets.js
+++ test/getTweets.js
@@ -11,6 +11,7 @@ const assert = require('assert');
 describe('Array', () => {
   describe('#indexOf()', () => {
     it('should return -1 when the value is not present', () => {
+      // COMMENT
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
