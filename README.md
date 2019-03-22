e2609c9b0814e1a758bb634c2f773d15f5a5c384
Marko Kosir <kosir.mar@gmail.com>
2019-03-22 14:05:07 +0100

    readme update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # twit-wrapper
 
-[![npm (scoped)](https://img.shields.io/npm/v/@bamblehorse/tiny.svg)](https://www.npmjs.com/package/@marko424/twit-wrapper)
-[![npm bundle size (minified)](https://img.shields.io/bundlephobia/min/@bamblehorse/tiny.svg)](https://www.npmjs.com/package/@marko424/twit-wrapper)
+[![npm (scoped)](https://img.shields.io/npm/v/@marko424/twit-wrapper.svg)](https://www.npmjs.com/package/@marko424/twit-wrapper)
+[![npm bundle size (minified)](https://img.shields.io/bundlephobia/min/@marko424/twit-wrapper.svg)](https://www.npmjs.com/package/@marko424/twit-wrapper)
 
 ## Highly opinionated Twitter API client for Node.js
 
@@ -10,7 +10,7 @@ It is implemented with facade pattern which translates twit library's existing i
 
 ## Install
 
-```
+```shell
 npm i @marko424/twit-wrapper
 ```
 
@@ -19,22 +19,22 @@ npm i @marko424/twit-wrapper
 Only most commonly used function for interacting with Twitter API are implemented:
 
 - postTweet(msg)
-- getTweets(keyword, fromDate)
+- getTweets(keyword, fromDate, lang = 'en', maxResults = 100)
 
-```
+```js
 const TwitterClient = require('TwitterClient');
-const twitterClient = new TwitterClient();
+const twitterClient = new TwitterClient(consumerKey, consumerSecret, accessToken, accessTokenSecret);
 
 try {
   const msgToPost = 'Post a test message';
   const postedMsg = await twitterClient.postTweet(msgToPost);
-  console.log(postedMsg)
+  console.log(postedMsg);
 } catch (e) {
   console.error(e);
 }
 
 try {
-  const searchedTweets = await twitterClient.searchTweets('basketball', '2017-01-01');
+  const searchedTweets = await twitterClient.getTweets('basketball', '2017-01-01');
   console.log(searchedTweets);
 } catch (e) {
   console.error(e);
---
+++ src/TwitterClient.js
@@ -0,0 +1,65 @@
+const Twit = require('twit');
+
+class TwitterClient {
+  constructor(consumerKey, consumerSecret, accessToken, accessTokenSecret, timeout = 60000, strictSSL = true) {
+    this._twit = new Twit({
+      consumer_key: consumerKey,
+      consumer_secret: consumerSecret,
+      access_token: accessToken,
+      access_token_secret: accessTokenSecret,
+      timeout_ms: timeout,
+      strictSSL: strictSSL,
+    });
+  }
+
+  //// Methods
+  postTweet(msg) {
+    return new Promise((resolve, reject) => {
+      const url = 'statuses/update';
+      const params = { status: `${msg}` };
+      this._twit
+        .post(url, params)
+        .then(res => {
+          const tweet = res.data;
+          resolve({
+            tweet_created_at: tweet.created_at,
+            tweet_user_name: tweet.user.name,
+            tweet_user_screen_name: tweet.user.screen_name,
+            tweet_text_length: tweet.text.length,
+            tweet_text: tweet.text,
+          });
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
+  }
+  // Return tweets containing the keyword since fromDate param.
+  // Filter:
+  // - default language is English.
+  // - Limit result to 100 tweets (maximum)
+  getTweets(keyword, fromDate, lang = 'en', maxResults = 100) {
+    return new Promise((resolve, reject) => {
+      const url = 'search/tweets';
+      const params = { q: `${keyword} since:${fromDate}`, count: maxResults, lang: lang };
+      this._twit
+        .get(url, params)
+        .then(res => {
+          const tweets = res.data.statuses;
+          const tweetsMap = tweets.map(tweet => ({
+            created_at: tweet.created_at,
+            user_name: tweet.user.name,
+            user_screen_name: tweet.user.screen_name,
+            tweet_text: tweet.text,
+            tweet_retweeted_status_text: tweet.retweeted_status ? tweet.retweeted_status.text : null,
+          }));
+          resolve(tweetsMap);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
+  }
+}
+
+module.exports = TwitterClient;
