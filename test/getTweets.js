1d3088ae6afd79f0e2d25d1263fecee6f27e4585
Marko Kosir <kosir.mar@gmail.com>
2019-03-23 12:22:03 +0100

    include dummy test

--- test/getTweets.js
+++ test/getTweets.js
@@ -6,20 +6,19 @@
 //   accessTokenSecret,
 // );
 
-const assert = require('assert');
-
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      // COMMENT
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
-
 // describe('getTweets()', () => {
 //   it("Return 100 tweets containing the keyword 'javascript'", async () => {
 //     const searchedTweets = await twitterClient.getTweets('javascript', '2017-01-01');
 //     searchedTweets.should.have.length(100);
 //   });
 // });
+
+const assert = require('assert');
+
+describe('Dummy', () => {
+  describe('Test array function indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
